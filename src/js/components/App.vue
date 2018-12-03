54730eddd8970af19478e05440a42cd2388ecc0a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2018-12-03 00:26:21 -0800

    Create vuex state store folder and file.

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -1,8 +1,19 @@
 <template>
+  <div>
+    <h1 :title="title">{{ this.title }}</h1>
+  </div>
 </template>
 
 
 <script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      title: 'eli-vue-scaffold'
+    }
+  }
+}
 </script>
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue';
 import App from './components/App.vue';
+import Vuex from 'vuex'
+import store from './store'
+
+Vue.use(Vuex)
 
 var app = new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  store
 });
